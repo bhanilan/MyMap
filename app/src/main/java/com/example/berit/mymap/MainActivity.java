c1c1d4b7d9889e23d883343bae0ac412a3a9591e
Mart Naris <mart.naris@hotmail.com>
2016-05-09 16:43:01 +0300

    Update

--- app/src/main/java/com/example/berit/mymap/MainActivity.java
+++ app/src/main/java/com/example/berit/mymap/MainActivity.java
@@ -6,7 +6,21 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback{
+
+    private final static String TAG = "MainActivity";
+
+    //saving map as variable
+    private GoogleMap mGoogleMap;
+
+    private Menu mOptionsMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,27 +28,66 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.mymap);
+        mapFragment.getMapAsync(this);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+        mOptionsMenu = menu;
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        switch (item.getItemId()){
+            case R.id.menu_maptype_normal:
+            case R.id.menu_maptype_hybrid:
+            case R.id.menu_maptype_satellite:
+            case R.id.menu_maptype_terrain:
+            case R.id.menu_maptype_none:
+                item.setChecked(true);
+                changeMapType();
+                return true;
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void changeMapType(){
+        if(mGoogleMap == null){
+            return;
         }
 
-        return super.onOptionsItemSelected(item);
+        if(mOptionsMenu.findItem(R.id.menu_maptype_hybrid).isChecked()){
+            mGoogleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        } else if(mOptionsMenu.findItem(R.id.menu_maptype_normal).isChecked()){
+            mGoogleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        } else if(mOptionsMenu.findItem(R.id.menu_maptype_none).isChecked()){
+            mGoogleMap.setMapType(GoogleMap.MAP_TYPE_NONE);
+        } else if(mOptionsMenu.findItem(R.id.menu_maptype_satellite).isChecked()){
+            mGoogleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        } else if(mOptionsMenu.findItem(R.id.menu_maptype_terrain).isChecked()){
+            mGoogleMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+        }
+    }
+
+    @Override
+    //my key has been checked
+    public void onMapReady(GoogleMap googleMap) {
+        mGoogleMap = googleMap;
+
+        LatLng latLng = new LatLng(59.3955457, 24.6643051);
+        mGoogleMap.addMarker(new MarkerOptions().position(latLng).title("ITK"));
+
+        // set zoom level to 15 - street
+        mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 17));
+
+        mGoogleMap.setMyLocationEnabled(true);
     }
 }
